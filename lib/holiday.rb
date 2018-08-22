9d0091338b55b16b3ae6289144e66b0334880f93
github username <github email address>
2018-08-21 20:02:17 -0400

    More practice traversing through nested data structures.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
@@ -25,23 +26,33 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
 
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
+
+  holiday_hash
+
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash[season][holiday_name] = supply_array
+
+  holiday_hash
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
 
 end
 
@@ -53,18 +64,60 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  text = ""
+  holiday_hash.each do |season, holiday|
 
+    text << "#{season.capitalize}:"
+    text << "\n"
+
+    holiday.each do |key, supplies|
+      text << "  "
+      temp_string = key.to_s
+      if !(temp_string.include?("_"))
+        text << temp_string.capitalize
+      else
+        temp_array = temp_string.split("_")
+        index = 0
+        while index < temp_array.length do
+          temp_array[index] = temp_array[index].capitalize
+          index += 1
+        end
+        text << temp_array.join(" ")
+      end
+      text << ": "
+
+      index = 0
+      while index < supplies.length
+        if (index + 1) == supplies.length
+          text << supplies[index]
+          text << "\n"
+        else
+          text << "#{supplies[index]}, "
+        end
+        index += 1
+      end
+    end
+  end
+  puts text
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_names = Array.new
+  holiday = ""
 
+  holiday_hash.each do |season, holiday|
+    holiday.each do |key, supplies|
+      index = 0
+      while index < supplies.length()
+        if supplies[index] == "BBQ"
+          holiday = key
+          holiday_names.push(holiday)
+        end
+        index += 1
+      end
+    end
+  end
+  holiday_names
 end
-
-
-
-
-
-
-
